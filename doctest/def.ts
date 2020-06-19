b98065e2158e613bfb3462e318a0774e18a8f3cc
askirmas <kirmas@phystech.edu>
2020-06-20 02:32:35 +0300

    Change doctype test to not mix meta description and function IO

--- doctest/def.ts
+++ doctest/def.ts
@@ -2,14 +2,18 @@ import schema from "./schema.json"
 
 export type eStatus = keyof typeof schema["definitions"]["Status"]["definitions"]
 
-export type Suite<A, R> = [eStatus, string, A, R, R?, A?]
+export type Suite<A, R> = [A, R, R?, A?]
 
 export type Monad = (arg: any) => any
 
 export type DocTest<F extends Monad> = {
   $schema: string
 } & {
-  [Topic: string]: Array<string|Suite<Parameters<F>[0], ReturnType<F>>>
+  [Topic: string]: Array<string|[
+    eStatus,
+    string,
+    Suite<Parameters<F>[0], ReturnType<F>>
+  ]>
 }
 
 const statusesTrue = new Set<eStatus>(["DONE", "LEG", "DEPR", "BUG-", "UNST"]) 
